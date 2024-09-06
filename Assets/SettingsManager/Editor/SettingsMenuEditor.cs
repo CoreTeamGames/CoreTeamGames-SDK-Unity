b2302019768d8b4b5999596b1a528350c47f36a3
GoustYTC <goustytc@gmail.com>
2024-09-06 19:35:40 +0700

    SettingsService update

---
+++ Assets/SettingsManager/Editor/SettingsMenuEditor.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine.UI;
+using TMPro;
+
+[CustomEditor(typeof(SettingsMenu))]
+public class SettingsMenuEditor : Editor
+{
+    private ReorderableList _valuesList;
+    private SettingsMenu _menu;
+    private float SingleLineHeight => EditorGUIUtility.singleLineHeight;
+
+    private void OnEnable()
+    {
+_menu = (SettingsMenu)target;
+
+        _valuesList = new ReorderableList(serializedObject,
+                serializedObject.FindProperty("_settingsValuesWithUIElements"),
+                false, true, false, false);
+
+
+        _valuesList.elementHeight = SingleLineHeight * 4 + 4;
+        _valuesList.drawHeaderCallback =
+        (Rect rect) =>
+        {
+            EditorGUI.LabelField(new Rect(rect.x, rect.y, rect.width, SingleLineHeight), $"Settings Values");
+        };
+        _valuesList.drawElementCallback =
+        (Rect rect, int index, bool isActive, bool isFocused) =>
+        {
+            rect.y += 2;
+            var _element = _valuesList.serializedProperty.GetArrayElementAtIndex(index);
+            var _uiElement = _element.FindPropertyRelative("_uiElement");
+            var _value = _element.FindPropertyRelative("_value");
+
+            EditorGUI.LabelField(new Rect(rect.x, rect.y, rect.width, SingleLineHeight * 2), $"Value Name: {'"'}{_value.FindPropertyRelative("_valueName").stringValue}{'"'}.\nValue Type: {'"'}{_value.FindPropertyRelative("_valueType").enumDisplayNames[_value.FindPropertyRelative("_valueType").enumValueIndex]}{'"'}");
+            EditorGUI.PropertyField(new Rect(rect.x, rect.y + SingleLineHeight * 2, rect.width, SingleLineHeight), _element.FindPropertyRelative("_elementType"));
+            if (_element.FindPropertyRelative("_elementType").enumValueIndex == 0)
+            {
+                EditorGUI.ObjectField(new Rect(rect.x, rect.y + SingleLineHeight * 3, rect.width, SingleLineHeight), _uiElement, typeof(Toggle));
+            }
+            else if (_element.FindPropertyRelative("_elementType").enumValueIndex == 1)
+            {
+                EditorGUI.ObjectField(new Rect(rect.x, rect.y + SingleLineHeight * 3, rect.width, SingleLineHeight), _uiElement, typeof(TMP_InputField));
+            }
+            else if (_element.FindPropertyRelative("_elementType").enumValueIndex == 2)
+            {
+                EditorGUI.ObjectField(new Rect(rect.x, rect.y + SingleLineHeight * 3, rect.width, SingleLineHeight), _uiElement, typeof(TMP_Dropdown));
+            }
+            else if (_element.FindPropertyRelative("_elementType").enumValueIndex == 3)
+            {
+                EditorGUI.ObjectField(new Rect(rect.x, rect.y + SingleLineHeight * 3, rect.width, SingleLineHeight), _uiElement, typeof(Slider));
+            }
+
+        };
+    }
+
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        _valuesList.DoLayoutList();
+        if (GUILayout.Button("Update Content"))
+        {
+            _menu.AddSettingsValues();
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/SettingsManager/Scripts/SettingsIO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public static class SettingsIO
+{
+    public static void WriteSettings(SettingsValue[] settingsValues)
+    {
+        string str = JsonConvert.SerializeObject(settingsValues,Formatting.Indented);
+        Debug.Log(str);
+    }
+    public static List<SettingsValue> ReadSettings()
+    {
+return null;
+    }
+}
